e3979c3831e90be2a95c54afb5a4f676585b9404
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2022-08-02 23:50:42 +0530

    interface

---
+++ interfaces.java
@@ -0,0 +1,16 @@
+public class interfaces {
+    public static void main(String[] args){
+
+    }
+
+    //in interface by default all functions are abstract
+    // this gives us the basic concept
+    // other class can implement it
+    interface car{
+        void start();
+
+    }
+    interface person{
+        void walk();
+    }
+}
